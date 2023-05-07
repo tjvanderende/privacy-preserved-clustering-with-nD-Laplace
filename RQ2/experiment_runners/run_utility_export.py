c18a4587a19c85aae0d0fe4ac943dc69a9e86ef0
tjvanderende <tjvanderende@hotmail.com>
2023-05-07 17:04:00 +0000

    feat fixed a lot stuff

--- RQ2/experiment_runners/run_utility_export.py
+++ RQ2/experiment_runners/run_utility_export.py
@@ -3,7 +3,6 @@ from Helpers import UtilityPlotter
 import Helpers.helpers as helpers
 from sklearn.cluster import DBSCAN, AffinityPropagation, KMeans
 
-
 def models(): 
     return {
         'KMeans': KMeans(n_clusters=4, init='random', algorithm='lloyd'),
@@ -13,8 +12,8 @@ def models():
 
 # import export
 utility_metrics = helpers.load_dataset('./export/results/utility-3d.csv')
-print(utility_metrics.head())
+print(utility_metrics)
 # run experiments
 plotter = UtilityPlotter.UtilityPlotter('./export/plain.csv', models())
 plotter.plot_external_validation(utility_metrics, export_path='./export/results/', save=True)
-
+print('Done')
